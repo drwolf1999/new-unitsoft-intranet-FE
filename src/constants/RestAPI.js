6d3dbd927ec964b376e732913bb7c71a50774fb8
root <drwolf1888@gmail.com>
2019-12-29 15:35:23 +0900

    RestAPI setting again

--- src/constants/RestAPI.js
+++ src/constants/RestAPI.js
@@ -1,10 +1,5 @@
-const SERVER_DOMAIN = 'http://192.168.1.2:3000/';
-// const SERVER_DOMAIN = 'http://192.168.0.73:3000/';
-// const SERVER_DOMAIN = 'http://192.168.0.40:3000/';
-// const SERVER_DOMAIN = 'http://192.168.0.15:3000/';
-// const SMS_DOMAIN = 'https://sens.apigw.ntruss.com/sms/v2/services/';
+const SERVER_DOMAIN = 'http://108.61.181.24:701:8002/';
 
 export default {
     SERVER_DOMAIN: SERVER_DOMAIN,
-    // SMS_DOMAIN: SMS_DOMAIN,
 };
